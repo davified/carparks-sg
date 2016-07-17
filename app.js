842b7c8405124339e193195e1fbe1e6351d94b15
David Tan <davified@gmail.com>
2016-07-17 12:48:01 +0800

    amended metadata description

--- app.js
+++ app.js
@@ -4,23 +4,21 @@ const bodyParser = require('body-parser')
 const port = process.env.PORT || 3000
 const api = require('./api')
 // const favicon = require('serve-favicon')
-require('dotenv').config();
+// require('dotenv').config()
 
 var app = express()
 
-app.use(bodyParser.json())
-app.use(bodyParser.urlencoded({ extended: false }))
+// app.use(bodyParser.json())
+// app.use(bodyParser.urlencoded({ extended: false }))
 app.use(express.static(path.join(__dirname, 'public')))
 app.use(express.static('files'))
-app.use(function(req, res, next) {
-  res.header("Access-Control-Allow-Origin", "*");
-  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-  next();
-});
+app.use(function (req, res, next) {
+  res.header('Access-Control-Allow-Origin', '*')
+  res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept')
+  next()
+})
 
-/* index(map) views and api views */
 app.use('/', api)
-// app.use('/', ura)
 
 app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname + '/index.html'))
